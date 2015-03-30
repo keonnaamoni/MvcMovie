aec0f8c157e87b5344d1182734914d5058a4d8df
keonnaamoni <keonnaevans@gmail.com>
2015-03-30 11:53:20 -0400

    Adding Controller
    Change coding

--- MvcMovie/Controllers/HelloWorldControler.cs
+++ MvcMovie/Controllers/HelloWorldControler.cs
@@ -19,9 +19,9 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome()
+        public string Welcome(string name, int numTimes = 1)
         {
-            return "This is the Welcome action method...";
+            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
         }
     }
 }
