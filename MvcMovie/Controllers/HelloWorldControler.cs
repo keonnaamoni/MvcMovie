dd9a8bd957ea482d7d9af6f942b5133b8c4049bb
keonnaamoni <keonnaevans@gmail.com>
2015-03-30 12:56:27 -0400

    Controller
    Changing the coding from int numTimes to int ID

--- MvcMovie/Controllers/HelloWorldControler.cs
+++ MvcMovie/Controllers/HelloWorldControler.cs
@@ -19,9 +19,9 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/Welcome/ 
 
-        public string Welcome(string name, int numTimes = 1)
+        public string Welcome(string name, int ID = 1)
         {
-            return HttpUtility.HtmlEncode("Hello " + name + ", NumTimes is: " + numTimes);
+            return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
         }
     }
 }
