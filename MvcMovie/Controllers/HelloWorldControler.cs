a2483c927862ee3f6a21809ee3760912bb044970
keonnaamoni <keonnaevans@gmail.com>
2015-03-30 11:34:00 -0400

    Added Controller
    HelloWorldController

---
+++ MvcMovie/Controllers/HelloWorldControler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldControler : Controller
+    {
+        // GET: HelloWorldControler
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
