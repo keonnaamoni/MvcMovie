dc600a99a427d501575a09b30007e0b9f40a5c13
keonnaamoni <keonnaevans@gmail.com>
2015-04-01 11:05:06 -0400

    Changed date format

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -14,7 +14,7 @@ namespace MvcMovie.Models
         public string Title { get; set; }
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
